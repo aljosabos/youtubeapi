6f35cf9b457a75ca106ee8f9d725206c7084a3e1
Aljosa Boskovic <aljosabos@gmail.com>
2023-03-13 10:18:44 +0100

    add basic folder structure, install material-ui

---
+++ src/layouts/Dashboard/Dashboard.tsx
@@ -0,0 +1,16 @@
+import Home from "../../pages/Home/Home";
+import Sidebar from "../../pages/Home/Sidebar/Sidebar";
+import Videos from "../../pages/Home/Videos/Videos";
+import Header from "../Header/Header";
+
+function Dashboard() {
+  return (
+    <div>
+      <Header />
+      <Sidebar />
+      <Videos />
+    </div>
+  );
+}
+
+export default Dashboard;
