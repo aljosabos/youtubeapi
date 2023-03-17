a4557343071aa64358db2406e235b1007bc24a22
Aljosa Boskovic <aljosabos@gmail.com>
2023-03-17 15:17:36 +0100

    add video component, display video components mock on the home page

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -1,3 +1,34 @@
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import "./Video.scss";
+
 export default function Video() {
-  return <div>Video</div>;
+  return (
+    <div className="Video">
+      <Card>
+        <CardMedia
+          sx={{ height: 100 }}
+          image="https://i.ytimg.com/vi/wHff7sABAvs/hqdefault.jpg?sRUAAIhCGAE=&rs=AOn4CLDoJkJIqUoNSuMjraytDhEFSWrWVw"
+          title="video"
+          className="Video__image"
+        />
+        <CardContent className="Video__content">
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="div"
+            className="Video__content-heading"
+          >
+            video
+          </Typography>
+          {/* <Typography variant="body2" color="text.secondary">
+            great video just to see
+          </Typography> */}
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
