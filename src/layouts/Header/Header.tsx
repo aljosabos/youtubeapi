44bd0fcbd1a9cae0b1bd0be3bd262d0998a614b4
Aljosa Boskovic <aljosabos@gmail.com>
2023-03-20 16:09:37 +0100

    conditionaly render sign in or sign out button, save access token in local storage

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -4,22 +4,51 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import YoutubeLogo from "../../images/youtube.png";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import { useGoogleLogin } from "@react-oauth/google";
+import { googleLogout } from "@react-oauth/google";
+import { useState } from "react";
 
 export default function Header() {
-  const handleSignIn = useGoogleLogin({
-    onSuccess: (tokenResponse) => console.log(tokenResponse),
+  const [accessToken, setAccessToken] = useState<string | null>(
+    localStorage.getItem("access_token")
+  );
+
+  const login = useGoogleLogin({
+    onSuccess: ({ access_token }) => {
+      localStorage.setItem("access_token", access_token);
+      setAccessToken(access_token);
+    },
   });
+
+  const clearToken = () => {
+    localStorage.removeItem("access_token");
+    setAccessToken(null);
+  };
+
+  console.log(accessToken);
 
   return (
     <div className="Header">
       <img src={YoutubeLogo} className="Header__logo" alt="youtube_logo" />
       <SearchBar />
-      <Button
-        startIcon={AccountCircleIcon}
-        text="Sign in"
-        className="Header__button"
-        onClick={handleSignIn}
-      />
+
+      {!accessToken ? (
+        <Button
+          startIcon={AccountCircleIcon}
+          text="Sign in"
+          className="Header__button"
+          onClick={login}
+        />
+      ) : (
+        <Button
+          startIcon={AccountCircleIcon}
+          text="Sign out"
+          className="Header__button"
+          onClick={() => {
+            googleLogout();
+            clearToken();
+          }}
+        />
+      )}
     </div>
   );
 }
