7eb6f779c7e2267650fcd9dd0756cb234990d115
Aljosa Boskovic <aljosabos@gmail.com>
2023-03-20 15:00:02 +0100

    add google auth library, get access token

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -3,8 +3,13 @@ import "./Header.scss";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import YoutubeLogo from "../../images/youtube.png";
 import SearchBar from "../../components/SearchBar/SearchBar";
+import { useGoogleLogin } from "@react-oauth/google";
 
 export default function Header() {
+  const handleSignIn = useGoogleLogin({
+    onSuccess: (tokenResponse) => console.log(tokenResponse),
+  });
+
   return (
     <div className="Header">
       <img src={YoutubeLogo} className="Header__logo" alt="youtube_logo" />
@@ -13,6 +18,7 @@ export default function Header() {
         startIcon={AccountCircleIcon}
         text="Sign in"
         className="Header__button"
+        onClick={handleSignIn}
       />
     </div>
   );
