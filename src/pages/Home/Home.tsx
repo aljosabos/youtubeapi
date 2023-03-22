2dea237d8c3503c8a435591e16af6d9e011d0f11
Aljosa Boskovic <aljosabos@gmail.com>
2023-03-22 16:05:44 +0100

    add fetch new videos on scroll

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -10,18 +10,22 @@ import "./Home.scss";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 export default function Home() {
-  const maxResults = 50;
+  const maxResults = 16;
   const [videos, setVideos] = useState<IVideoResponse[]>([]);
-
+  const [nextPageToken, setNextPageToken] = useState<string>("");
+  // let nextPageToken = "";
   // `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=50&key=${process.env.REACT_APP_API_KEY}
 
   const fetchVideos = async () => {
     try {
-      const videos = await axios.get(
-        `https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&fields=items(id,contentDetails(duration),snippet(title,channelTitle,publishedAt,thumbnails(high(url))),statistics(viewCount)),nextPageToken&maxResults=${maxResults}&key=${process.env.REACT_APP_API_KEY}`
+      const response = await axios.get(
+        `https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&fields=items(id,contentDetails(duration),snippet(title,channelTitle,publishedAt,thumbnails(high(url))),statistics(viewCount)),nextPageToken&maxResults=${maxResults}&pageToken=${nextPageToken}&key=${process.env.REACT_APP_API_KEY}`
       );
-      console.log(videos.data);
-      if (videos) setVideos(videos.data.items);
+      if (response) {
+        setVideos((current) => [...current, ...response.data.items]);
+        setNextPageToken(response.data.nextPageToken);
+        // nextPageToken = response.data.nextPageToken;
+      }
     } catch (error) {
       console.log(error);
     }
@@ -31,30 +35,36 @@ export default function Home() {
     fetchVideos();
   }, []);
 
-  const mockFetch = () => {
-    console.log("hey");
-  };
+  console.log(videos);
+
+  console.log(nextPageToken);
 
   return (
     <InfiniteScroll
-      scrollableTarget="scrollable_home"
-      dataLength={50}
-      next={mockFetch}
-      hasMore={true}
+      scrollableTarget="scrollable"
+      dataLength={videos.length}
+      next={fetchVideos}
+      hasMore={videos.length < 500}
       loader={<h4>Loading...</h4>}
+      className="Home"
+      endMessage={
+        <p style={{ textAlign: "center" }}>
+          <b>Yay! You have seen it all</b>
+        </p>
+      }
     >
-      <div className="Home" id="scrollable_home">
-        {videos.map((video: IVideoResponse) => (
-          <Video
-            key={video.id}
-            title={video.snippet.title}
-            channel={video.snippet.channelTitle}
-            image={video.snippet.thumbnails.high.url}
-            duration={formatISOtoHumanReadable(video.contentDetails.duration)}
-            views={formatToThousands(Number(video.statistics.viewCount))}
-          />
-        ))}
-      </div>
+      {/* <div className="Home" id="scrollable"> */}
+      {videos.map((video: IVideoResponse, index) => (
+        <Video
+          key={index}
+          title={video.snippet.title}
+          channel={video.snippet.channelTitle}
+          image={video.snippet.thumbnails.high.url}
+          duration={formatISOtoHumanReadable(video.contentDetails.duration)}
+          views={formatToThousands(Number(video.statistics.viewCount))}
+        />
+      ))}
+      {/* </div> */}
     </InfiniteScroll>
   );
 }
