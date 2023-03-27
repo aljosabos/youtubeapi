196c8bc65813dce07b3bac2a19eb3095d3339087
Aljosa Boskovic <aljosabos@gmail.com>
2023-03-27 15:07:59 +0200

    add InfiniteScroll library component inside subscriptions list

--- src/layouts/Sidebar/Subscriptions/Subscriptions.tsx
+++ src/layouts/Sidebar/Subscriptions/Subscriptions.tsx
@@ -7,14 +7,15 @@ import { getSubscriptionsThunk } from "../../../redux/thunks/subscriptionsThunk"
 import Button from "../../../components/Button/Button";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import ExpandLessIcon from "@mui/icons-material/ExpandLess";
-import { COLLAPSED_SUBSCRIPTIONS_NUM } from "../../../data/constants";
+import { COLLAPSED_SUBSCRIPTIONS_NUM, SCROLLABLE_JSX } from "../../../data/constants";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default function Subscriptions() {
   const [shouldExpandSubscriptions, setShouldExpandSubscriptions] = useState<boolean>(false);
   const dispatch = useAppDispatch();
   const subscriptionsStatus = useAppSelector(subscriptionsStatusSelector);
   const subscriptions = useAppSelector(subscriptionsSelector);
-  const [totalCount, setTotalCount] = useState<number>();
+  const [totalCount, setTotalCount] = useState<number>(0);
   const listRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
@@ -22,7 +23,7 @@ export default function Subscriptions() {
   }, [subscriptionsStatus, dispatch]);
 
   useEffect(() => {
-    if (subscriptions) setTotalCount(Number(subscriptions[0].contentDetails.totalItemCount));
+    if (subscriptions.length > 0) setTotalCount(Number(subscriptions[0]?.contentDetails.totalItemCount));
   }, [subscriptions]);
 
   const scrollListToTop = () => {
@@ -41,13 +42,31 @@ export default function Subscriptions() {
 
   const btnText = shouldExpandSubscriptions ? "Show less" : `Show ${totalCount && totalCount - COLLAPSED_SUBSCRIPTIONS_NUM} more`;
 
+  const loadMore = () => {
+    console.log("load more");
+  };
+
   return (
     <div className="Subscriptions">
-      <div className={`Subscriptions__list ${shouldExpandSubscriptions && "Subscriptions__list-expanded"}`} ref={listRef}>
-        {subscriptions.map((subscription, index) => (
-          <Subscription image={subscription.snippet.thumbnails.high.url} title={subscription.snippet.title} key={index} channelId={subscription.snippet.channelId} />
-        ))}
+      <div id={SCROLLABLE_JSX} className={`Subscriptions__list ${shouldExpandSubscriptions && "Subscriptions__list-expanded"}`} ref={listRef}>
+        <InfiniteScroll
+          dataLength={totalCount}
+          next={loadMore}
+          hasMore={true}
+          scrollableTarget={SCROLLABLE_JSX}
+          loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+        >
+          {subscriptions.map((subscription, index) => (
+            <Subscription image={subscription.snippet.thumbnails.high.url} title={subscription.snippet.title} key={index} channelId={subscription.snippet.channelId} />
+          ))}
+        </InfiniteScroll>
       </div>
+
       <Button onClick={toggleExpandSubscriptions} text={btnText} startIcon={shouldExpandSubscriptions ? ExpandLessIcon : ExpandMoreIcon} className="Subscriptions__btn" />
     </div>
   );
