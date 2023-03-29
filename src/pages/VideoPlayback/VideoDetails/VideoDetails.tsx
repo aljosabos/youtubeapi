f1e927ebc6e1f0f11de50cee40f5ad2ccf04868e
Aljosa Boskovic <aljosabos@gmail.com>
2023-03-29 16:38:05 +0200

    change videoDetails classes naming

--- src/pages/VideoPlayback/VideoDetails/VideoDetails.tsx
+++ src/pages/VideoPlayback/VideoDetails/VideoDetails.tsx
@@ -10,18 +10,18 @@ export default function VideoDetails({ id, title, channelId, channelTitle, descr
     <div className="VideoDetails">
       <h3 className="VideoDetails__title">{title} </h3>
 
-      <div className="VideoDetails__channel-info">
-        {<Avatar alt="Subscriptions avatar" src={thumbnails?.high?.url} className="VideoDetails__channel-info-avatar" />}
-        <div className="VideoDetails__channel-info-head">
-          <h4 className="VideoDetails__channel-info-head-name">{channelTitle}</h4>
-          <span className="VideoDetails__channel-info-head-subscribers">20K subscribers</span>
+      <div className="VideoDetails__channel">
+        {<Avatar alt="Subscriptions avatar" src={thumbnails?.high?.url} className="VideoDetails__channel-avatar" />}
+        <div className="VideoDetails__channel-text">
+          <h4 className="VideoDetails__channel-text-name">{channelTitle}</h4>
+          <span className="VideoDetails__channel-text-subscribers">20K subscribers</span>
         </div>
       </div>
 
-      <div className="VideoDetails__description">
-        <span className="VideoDetails__description-views">{formatNumToThousands(Number(statistics?.viewCount))}K views</span>
-        <span className="VideoDetails__description-time">{moment(publishedAt).fromNow()}</span>
-        <p className="VideoDetails__description-text">
+      <div className="VideoDetails__desc">
+        <span className="VideoDetails__desc-views">{formatNumToThousands(Number(statistics?.viewCount))}K views</span>
+        <span className="VideoDetails__desc-time">{moment(publishedAt).fromNow()}</span>
+        <p className="VideoDetails__desc-text">
           <Linkify
             componentDecorator={(decoratedHref: string, decoratedText: string, key: number) => (
               <a href={decoratedHref} key={key} target="_blank" rel="noreferrer">
@@ -34,7 +34,7 @@ export default function VideoDetails({ id, title, channelId, channelTitle, descr
           ;
         </p>
 
-        <span className="VideoDetails__description-tags">{tags}</span>
+        <span className="VideoDetails__desc-tags">{tags}</span>
       </div>
     </div>
   );
