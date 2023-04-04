ad5a58fd36d02dead7c3f49fb504a70f22ec8860
Aljosa Boskovic <aljosabos@gmail.com>
2023-04-04 12:42:34 +0200

    remove comments

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -7,8 +7,6 @@ export const LOAD_MORE_SIZE_PARAM = "&maxResults=4";
 
 export const POPULAR_VIDEOS_URL = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&fields=items(id,contentDetails(duration),snippet(title,channelTitle,publishedAt,thumbnails(high(url))),statistics(viewCount)),nextPageToken&key=${process.env.REACT_APP_API_KEY}`;
 
-// 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=shW9i6k8cB0%2CmO0OuR26IZM%2CPg9XSSeQIOc%2C1WEAJ-DFkHE%2CjRtLcPTbrxo%2C6umxqVvVK8Y%2CUqzKzugvtR0%2CCTXtXxD-TYg%2CTpLh9vB02MM%2Cz7n3SKwRaPA%2CFOvXEp-22f0%2C_ajQow29I7U%2CWKLD9QEx9ks%2C58Ofg9IHRN0%2CSZQhgExjBvQ%2CuGm4VIt14tU%2CvS3_72Gb-bI%2CZX3dJ_QVD0I%2CgvDWYQhYzhY%2CwgY9iO-fji8%2C5glbmwhyP-s%2CmmBl8Iz8OkE%2CLfaG3rGk3qE%2CJcl-Z9Lb8Hg%2CUJsGEZLYpKE%2CvVG3v8mCK1s%2Cr5x_Xog8fBQ%2C2elO1GEpVy0%2CjVXwCSjoVkI%2CHncXcsw5lZI%2C-4Tzalk090Y%2CJ5QeOa4pmBk%2C0bwD2PofI7I%2CVK_8nDq1A50%2CuV2y_co7WgY%2Cgw7gyYgewcc%2Cc27ZUhymHS8%2C3U467XfT6cY%2CxjIcsrj_PZ0%2CQSfVN4kD9c4%2CAkrht_QNNKs%2COSUGX3Ql4Ys%2CG8R4CbW4Jj0%2CvRYMf50sHPA%2CKPz1r8DCuH0%2CLxdvV0rLr7Y%2CSuGyZEuPKyI%2CcOOr33GnugY%2CKs1RBvvqlss&key=[YOUR_API_KEY]' \
-
 /* SUBSCRIPTION */
 export const SUBSCRIPTIONS_URL = `https://youtube.googleapis.com/youtube/v3/subscriptions?part=snippet&mine=true&maxResults=50&fields=items(id,snippet(title,channelId,thumbnails(high(url)))),nextPageToken,pageInfo(totalResults)&key=${process.env.REACT_APP_API_KEY}`;
 
--- src/pages/VideoPlayback/RelatedVideos/RelatedVideos.tsx
+++ src/pages/VideoPlayback/RelatedVideos/RelatedVideos.tsx
@@ -15,30 +15,6 @@ export default function RelatedVideos({ videoId }: IRelatedVideosProps) {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const relatedVideos = useAppSelector(relatedVideosSelector);
-  // const content = [];
-
-  // const response = axios
-  //   .get(
-  //     `https://www.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=4D_OwIyiTYU&type=video&pageToken&maxResults=30&key=${process.env.REACT_APP_API_KEY}`
-  //   )
-  //   .then((response) => console.log(response));
-
-  // const props = {
-  //   id: "1",
-  //   title: "Title",
-  //   image: "https://cdn.pixabay.com/photo/2015/06/28/11/25/climbing-824373_960_720.jpg",
-  //   channel: "Channel",
-  //   duration: "11:25",
-  //   views: 55,
-  //   publishDate: "3 days ago",
-  //   onClick: () => {
-  //     console.log("hello");
-  //   },
-  // };
-
-  // for (let i = 0; i < 30; i++) {
-  //   content.push(<VideoCard {...props} key={i} cardDirection="row" />);
-  // }
 
   useEffect(() => {
     if (videoId) dispatch(getRelatedVideosThunk(videoId));
