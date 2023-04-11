5f225394a0c5d456ad8f89d19df1236174c7ac65
Aljosa Boskovic <aljosabos@gmail.com>
2023-04-11 10:11:53 +0200

    update mock adapter to use size constants

--- src/utils/tests.data.ts
+++ src/utils/tests.data.ts
@@ -5,19 +5,22 @@ import {
   moreRecommendedVideosMock,
   recommendedVideosMock,
 } from "./recommendedVideosMock";
+import { INITIAL_LOAD_SIZE, LOAD_MORE_SIZE } from "../constants/constants";
 
 export const mockAxiosRequests = () => {
   const mock = new MockAdapter(axios);
 
-  mock.onGet(POPULAR_VIDEOS_URL, { params: { maxResults: 16 } }).reply(200, {
-    items: recommendedVideosMock.data.items,
-    nextPageToken: recommendedVideosMock.data.nextPageToken,
-  });
+  mock
+    .onGet(POPULAR_VIDEOS_URL, { params: { maxResults: INITIAL_LOAD_SIZE } })
+    .reply(200, {
+      items: recommendedVideosMock.data.items,
+      nextPageToken: recommendedVideosMock.data.nextPageToken,
+    });
 
   mock
     .onGet(POPULAR_VIDEOS_URL, {
       params: {
-        maxResults: 4,
+        maxResults: LOAD_MORE_SIZE,
         pageToken: recommendedVideosMock.data.nextPageToken,
       },
     })
