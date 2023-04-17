d16ed4e4ec5a1cdf174edd94792cb7add947c65c
Aljosa Boskovic <aljosabos@gmail.com>
2023-04-17 11:35:24 +0200

    add type for changeLanguage action

--- src/redux/slices/settingsSlice.ts
+++ src/redux/slices/settingsSlice.ts
@@ -1,21 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { ISettingsState } from "../types/settingsState";
+import { IChangeLanguageAction, ISettingsState } from "../types/settingsState";
 import { RootState } from "../store";
 
 const initialState: ISettingsState = {
   data: {
     language: "",
   },
-
-  status: "idle",
-  error: {},
 };
 
 export const settingsSlice = createSlice({
   name: "settings",
   initialState,
   reducers: {
-    changeLanguage(state, action) {
+    changeLanguage(state, action: IChangeLanguageAction) {
       state.data.language = action.payload;
     },
   },
@@ -23,5 +20,6 @@ export const settingsSlice = createSlice({
 
 export const languageSelector = (state: RootState) =>
   state.settings.data.language;
+
 export const { changeLanguage } = settingsSlice.actions;
 export default settingsSlice.reducer;
--- src/redux/types/settingsState.ts
+++ src/redux/types/settingsState.ts
@@ -1,9 +1,10 @@
-import { SerializedError } from "@reduxjs/toolkit";
-
 export interface ISettingsState {
   data: {
     language: string;
   };
-  status: "idle" | "loading" | "succeeded" | "failed";
-  error: SerializedError;
+}
+
+export interface IChangeLanguageAction {
+  payload: string;
+  type: string;
 }
