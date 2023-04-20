b1273291a895c8e8d9c1be04b5dd454bd7c47740
Aljosa Boskovic <aljosabos@gmail.com>
2023-04-20 11:49:12 +0200

    change styling for header icon

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -4,7 +4,7 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import YoutubeLogo from "../../images/youtube.png";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import { useGoogleLogin } from "@react-oauth/google";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../context/UserContext";
 import { AUTH_SCOPE } from "../../constants/endpointConstants";
 import { useNavigate } from "react-router";
@@ -23,6 +23,12 @@ export default function Header({ handleLogout, toggleExpandDrawer }: IHeaderProp
   const navigate = useNavigate();
   const { isLoggedIn, setIsLoggedIn } = useContext(UserContext);
   const { t } = useTranslation();
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const btnText = {
+    signIn: windowWidth > 1200 ? t("header.btn.signIn") : "",
+    signOut: windowWidth > 1200 ? t("header.btn.signOut") : "",
+  };
 
   const login = useGoogleLogin({
     onSuccess: (response) => {
@@ -36,6 +42,17 @@ export default function Header({ handleLogout, toggleExpandDrawer }: IHeaderProp
     navigate("/");
   };
 
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="Header">
       <span onClick={toggleExpandDrawer} className={"Header__menu"}>
@@ -46,17 +63,11 @@ export default function Header({ handleLogout, toggleExpandDrawer }: IHeaderProp
       <SearchBar />
 
       {!isLoggedIn ? (
-        <Button
-          startIcon={AccountCircleIcon}
-          text={t("header.btn.signIn") || ""}
-          className="Header__btn"
-          wrapperClassName="Header__btn-wrapper"
-          onClick={login}
-        />
+        <Button startIcon={AccountCircleIcon} text={btnText.signIn} className="Header__btn" wrapperClassName="Header__btn-wrapper" onClick={login} />
       ) : (
         <Button
           startIcon={AccountCircleIcon}
-          text={t("header.btn.signOut") || ""}
+          text={btnText.signOut}
           className="Header__btn"
           wrapperClassName="Header__btn-wrapper"
           onClick={handleLogout}
