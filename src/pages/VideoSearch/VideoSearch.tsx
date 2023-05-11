5d464ff22177b45c47bbf47c268f0013ef17b0af
Aljosa Boskovic <aljosabos@gmail.com>
2023-05-11 13:05:34 +0200

    disable horizontal video card view on small screen in video search

--- src/pages/VideoSearch/VideoSearch.tsx
+++ src/pages/VideoSearch/VideoSearch.tsx
@@ -4,10 +4,14 @@ import { useAppSelector } from "../../hooks/reduxHooks";
 import { IVideo } from "../../types/types";
 import "./VideoSearch.scss";
 import { searchedVideosSelector } from "../../redux/slices/searchVideosSlice";
+import { useWindowResize } from "../../hooks/useWindowResize";
+import { LARGE_WIDTH, MEDIUM_WIDTH } from "../../constants/constants";
 
 export default function VideoSearch() {
   const navigate = useNavigate();
   const searchedVideos = useAppSelector(searchedVideosSelector);
+  const { isResized } = useWindowResize(MEDIUM_WIDTH);
+  const layout = isResized ? undefined : "horizontal";
 
   const openVideoURL = (videoId: string) => {
     navigate(`/video/${videoId}`);
@@ -18,7 +22,7 @@ export default function VideoSearch() {
       <div className="VideoSearch__card-list">
         {searchedVideos?.map((video: IVideo, index) => (
           <VideoCard
-            layout="horizontal"
+            layout={layout}
             wrapperClassName="VideoSearch__card"
             ariaLabel="video-card"
             {...video}
